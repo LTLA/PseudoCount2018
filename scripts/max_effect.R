24bfdcd5a15c2e73ade00fe789131dccb281791c
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-07-15 14:53:52 +0100

    Updated max effect script to use new function.

--- scripts/max_effect.R
+++ scripts/max_effect.R
@@ -3,7 +3,6 @@
 source("functions.R")
 all.sf <- c(0.01, 0.1, 1, 10, 100)
 col <- viridis::viridis(length(all.sf))
-mus <- 10^seq(-3, 2, length.out=50)
 
 set.seed(1000)
 pdf("pics/max_effect.pdf")
@@ -12,7 +11,7 @@ for (disp in c(0, 0.1, 1, 10)) {
 
     for (i1 in seq_along(all.sf)) {
         for (i2 in seq_len(i1)) {
-            output[i1, i2] <- max(abs(SIMULATE_ERR(s1=all.sf[i1], s2=all.sf[i2], mu=mus, size=1/disp)))
+            output[i1, i2] <- FIND_MAX_ERR(s1=all.sf[i1], s2=all.sf[i2], size=1/disp)
         }
     }
 
