d49b83609e50807bc5a25034929a0c4a34029bf3
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-08 13:30:15 +0100

    Repeated spurious DE simulations across iterations for stability.

--- scripts/spurious_sim.R
+++ scripts/spurious_sim.R
@@ -36,24 +36,42 @@ dispersions <- c(0, 0.1, 1, 10)
 set.seed(1000)
 pdf("pics/max_effect.pdf")
 for (disp in dispersions) { 
-    output <- matrix(0, length(all.sf), length(all.sf))
 
-    for (i1 in seq_along(all.sf)) {
-        for (i2 in seq_len(i1)) {
-            output[i1, i2] <- FIND_MAX_ERR(s1=all.sf[i1], s2=all.sf[i2], size=1/disp)
+    collected <- vector("list", 10)
+    for (it in seq_along(collected)) {
+        output <- matrix(0, length(all.sf), length(all.sf))
+        for (i1 in seq_along(all.sf)) {
+            for (i2 in seq_len(i1)) {
+                output[i1, i2] <- FIND_MAX_ERR(s1=all.sf[i1], s2=all.sf[i2], size=1/disp)
+            }
+        }
+        collected[[it]] <- output
+    }
+
+    collected <- do.call(abind::abind, c(collected, list(along=3)))
+    output.mean <- output.se <- matrix(0, length(all.sf), length(all.sf))
+    for (i in seq_along(all.sf)) {
+        for (j in seq_len(i)) {
+            current <- collected[i,j,]
+            output.mean[i,j] <- mean(current)
+            output.se[i,j] <- sd(current)/sqrt(length(current))
         }
     }
 
-    output <- output/log(2)
-    plot(all.sf, numeric(length(all.sf)), ylim=range(output), type="n", log="x",
+    output.mean <- output.mean/log(2)
+    output.se <- output.se/log(2)
+    upper <- output.mean + output.se
+
+    plot(all.sf, numeric(length(all.sf)), ylim=range(upper), type="n", log="x",
         xlab="First size factor", ylab=expression("Maximum difference in"~log[2]*"-expression"),
         main=sprintf("Dispersion of %s", disp))
 
     for (i in seq_len(nrow(output))) {
         current <- seq_len(i)
-        lines(all.sf[current], output[i,current], lwd=2)
-        lines(all.sf[current], output[i,current], lwd=1.5, col=col[i])
-        points(all.sf[current], output[i,current], pch=21, bg=col[i], cex=1.5)
+        cur.mean <- output.mean[i,current]
+        lines(all.sf[current], cur.mean, lwd=2)
+        lines(all.sf[current], cur.mean, lwd=1.5, col=col[i])
+        points(all.sf[current], cur.mean, pch=21, bg=col[i], cex=1.5)
     }
     legend("topright", fill=col, legend=all.sf, title="Second size factor") 
 }
