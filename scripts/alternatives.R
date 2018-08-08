165767d62bad221deafa2c22b851988ae2ae897d
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-08 14:36:54 +0100

    Lowered means in alternative simulation for consistency.

--- scripts/alternatives.R
+++ scripts/alternatives.R
@@ -2,7 +2,7 @@
 
 set.seed(99999)
 ncells <- 500
-lambda <- rep(c(1, 100), each=ncells/2)
+lambda <- rep(c(0.1, 10), each=ncells/2)
 ngenes <- 10000
 
 PLOT_FUN <- function(g1, g2, main) {
@@ -21,19 +21,21 @@ for (scenario in 1:2) {
         pdf("pics/alternative_nb.pdf", width=4, height=6)
     }
 
-    size.fac <- colSums(X)
+    size.fac <- lambda
     size.fac <- size.fac/mean(size.fac)
+    first.group <- lambda==0.1
+    second.group <- !first.group
 
     # Square rooting.
     rooted <- sqrt( t(t(X)/size.fac) )
-    g1 <- rowMeans(rooted[,lambda==1])
-    g2 <- rowMeans(rooted[,lambda==100])
+    g1 <- rowMeans(rooted[,first.group])
+    g2 <- rowMeans(rooted[,second.group])
     PLOT_FUN(g1, g2, main="Square root")
         
     # Log-transform 
     log1p <- log2( t(t(X)/size.fac) + 1)
-    g1 <- rowMeans(log1p[,lambda==1])
-    g2 <- rowMeans(log1p[,lambda==100])
+    g1 <- rowMeans(log1p[,first.group])
+    g2 <- rowMeans(log1p[,second.group])
     PLOT_FUN(g1, g2, main="Log2-transform")
     
     # VST.
@@ -42,14 +44,14 @@ for (scenario in 1:2) {
     sizeFactors(dds) <- size.fac
     vst.out <- vst(dds, fitType="mean")
     
-    g1 <- rowMeans(assay(vst.out)[,lambda==1])
-    g2 <- rowMeans(assay(vst.out)[,lambda==100])
+    g1 <- rowMeans(assay(vst.out)[,first.group])
+    g2 <- rowMeans(assay(vst.out)[,second.group])
     PLOT_FUN(g1, g2, main="DESeq2 VST")
     
     # Our proposed approach.
     pseudo <- log2( t(t(X)/size.fac) + abs(diff(1/range(size.fac))) )
-    g1 <- rowMeans(pseudo[,lambda==1])
-    g2 <- rowMeans(pseudo[,lambda==100])
+    g1 <- rowMeans(pseudo[,first.group])
+    g2 <- rowMeans(pseudo[,second.group])
     PLOT_FUN(g1, g2, "Increased pseudo-count")
 
     dev.off()
@@ -60,7 +62,7 @@ for (scenario in 1:2) {
 
 library(SCnorm)
 ncells <- 500
-sf <- rep(c(1, 100), each=ncells/2)
+sf <- rep(c(0.1, 10), each=ncells/2)
 ngenes <- 10000
 abundances <- runif(ngenes, 0, 10)
 
