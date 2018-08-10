262d29e578272c3b0e148255a4a74afff0f3d41d
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-10 15:20:21 +0100

    Created effective MA plots for alternative transformations.

--- scripts/alternatives.R
+++ scripts/alternatives.R
@@ -1,12 +1,26 @@
-# Evaluating alternative transformations.
+# This script evaluates alternative transformations.
 
 set.seed(99999)
 ncells <- 500
-lambda <- rep(c(0.1, 10), each=ncells/2)
 ngenes <- 10000
+abundance <- 2^runif(ngenes, -2, 10)
 
-PLOT_FUN <- function(g1, g2, main) {
-    boxplot(list(Smaller=g1, Larger=g2), xlab="", ylab="Mean expression", main=main, col="grey80")
+# Specifying the size factor differences between cells.
+MULT <- rep(c(0.1, 10), each=ncells/2)
+first.group <- MULT==0.1
+second.group <- !first.group
+
+size.fac <- MULT 
+size.fac <- size.fac/mean(size.fac)
+
+# Central plotting function.
+par(cex.lab=1.4, cex.main=1.5)
+PLOT_FUN <- function(g1, g2, ...) {
+    Y <- g1-g2
+    X <- log10(abundance)
+    smoothScatter(X, Y, xlab=expression(Log[10]~"mean abundance"), ylab="Difference in group means", ...)
+    fit <- loess(Y ~ X, span=0.1)
+    curve(predict(fit, data.frame(X=x)), add=TRUE, col="red", lwd=2)
 }
 
 ###################################
@@ -14,29 +28,28 @@ PLOT_FUN <- function(g1, g2, main) {
 
 for (scenario in 1:2) {
     if (scenario==1L) {
-        X <- matrix(rpois(ngenes*ncells, lambda=lambda), ncol=ncells, byrow=TRUE)
+        lambda <- outer(abundance, size.fac, "*")
+        X <- matrix(rpois(ngenes*ncells, lambda=lambda), ncol=ncells)
+        ylim <- c(-3, 0.5)
         pdf("pics/alternative_pois.pdf", width=4, height=6)
     } else {
-        X <- matrix(rnbinom(ngenes*ncells, mu=lambda, size=1), ncol=ncells, byrow=TRUE)
+        lambda <- outer(abundance, size.fac, "*")
+        X <- matrix(rnbinom(ngenes*ncells, mu=lambda, size=1), ncol=ncells)
+        ylim <- c(-4, 0.5)
         pdf("pics/alternative_nb.pdf", width=4, height=6)
     }
-
-    size.fac <- lambda
-    size.fac <- size.fac/mean(size.fac)
-    first.group <- lambda==0.1
-    second.group <- !first.group
 
     # Square rooting.
     rooted <- sqrt( t(t(X)/size.fac) )
     g1 <- rowMeans(rooted[,first.group])
     g2 <- rowMeans(rooted[,second.group])
-    PLOT_FUN(g1, g2, main="Square root")
+    PLOT_FUN(g1, g2, main="Square root", ylim=ylim)
         
     # Log-transform 
-    log1p <- log2( t(t(X)/size.fac) + 1)
+    log1p <- log2( t(t(X)/size.fac) + 1 )
     g1 <- rowMeans(log1p[,first.group])
     g2 <- rowMeans(log1p[,second.group])
-    PLOT_FUN(g1, g2, main="Log2-transform")
+    PLOT_FUN(g1, g2, main="Log", ylim=ylim)
     
     # VST.
     library(DESeq2)
@@ -46,13 +59,13 @@ for (scenario in 1:2) {
     
     g1 <- rowMeans(assay(vst.out)[,first.group])
     g2 <- rowMeans(assay(vst.out)[,second.group])
-    PLOT_FUN(g1, g2, main="DESeq2 VST")
-    
+    PLOT_FUN(g1, g2, main="DESeq2 VST", ylim=ylim)
+
     # Our proposed approach.
     pseudo <- log2( t(t(X)/size.fac) + abs(diff(1/range(size.fac))) )
     g1 <- rowMeans(pseudo[,first.group])
     g2 <- rowMeans(pseudo[,second.group])
-    PLOT_FUN(g1, g2, "Increased pseudo-count")
+    PLOT_FUN(g1, g2, main="Increased pseudo-count", ylim=ylim)
 
     dev.off()
 }
